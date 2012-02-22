bc8a77227e2e00f318a68cdd56abdf45ac89a6b8
enricotempesti <entemp@tin.it>
2012-02-22 03:23:45 +0100

    aggiunta la pagina dei contatti e disegnato il form

--- src/Enrico/BlogBundle/Controller/PageController.php
+++ src/Enrico/BlogBundle/Controller/PageController.php
@@ -4,6 +4,9 @@
 namespace Enrico\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+// Import new namespaces
+use Enrico\BlogBundle\Entity\Enquiry;
+use Enrico\BlogBundle\Form\EnquiryType;
 
 class PageController extends Controller
 {
@@ -11,9 +14,36 @@ class PageController extends Controller
     {
         return $this->render('EnricoBlogBundle:Page:index.html.twig');
     }
+    
     public function aboutAction()
     {
         return $this->render('EnricoBlogBundle:Page:about.html.twig');
     }
+    
+   
+    public function contactAction()
+    {
+    $enquiry = new Enquiry();
+    $form = $this->createForm(new EnquiryType(), $enquiry);
+
+    $request = $this->getRequest();
+    if ($request->getMethod() == 'POST') {
+        $form->bindRequest($request);
+
+        if ($form->isValid()) {
+            // Perform some action, such as sending an email
+
+            // Redirect - This is important to prevent users re-posting
+            // the form if they refresh the page
+            return $this->redirect($this->generateUrl('EnricoBlogBundle_contact'));
+        }
+    }
+
+    return $this->render('EnricoBlogBundle:Page:contact.html.twig', array(
+        'form' => $form->createView()
+    ));
+    }
+    
+    
     
 }
